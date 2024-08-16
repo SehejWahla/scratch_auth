5efefce627ef085bcf2cd23c1af2b200e3ff0930
Sehej Wahla <sehejwahla@gmail.com>
2024-08-16 16:32:21 +1000

    mongo db setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModels.js";
 
 // @desc  Auth user/set token
 // route  POST /api/users/auth
@@ -7,4 +8,38 @@ const authUser = expressAsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc  Register new user
+// route  POST /api/users
+// @access Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User registered" });
+});
+
+// @desc  Log Out User
+// route  POST /api/users/logout
+// @access Private
+const logoutUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc  Get User Profile
+// route  GET /api/users/profile
+// @access Private
+const getUserProfile = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+// @desc  Update User Profile
+// route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
+
+connectDB();
 
 const port = process.env.PORT || 5001;
 
@@ -11,11 +14,11 @@ const app = express();
 
 app.use("/api/users", userRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
   res.send("server is running");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
